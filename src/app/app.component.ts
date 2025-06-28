1a370e49bf2dd86de2e9065636ff6f0a64197ec8
wTristaan <t.joubert78980@gmail.com>
2025-06-28 15:47:02 +0200

    [ADD] navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
