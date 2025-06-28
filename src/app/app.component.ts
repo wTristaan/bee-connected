03ebcf08dcb0bc973a0b58f64545b1758380d481
wTristaan <t.joubert78980@gmail.com>
2025-06-28 17:17:02 +0200

    [ADD] footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { BodyComponent } from './components/body/body.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [NavBarComponent, BodyComponent],
+  imports: [NavBarComponent, BodyComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
