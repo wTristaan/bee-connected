7c5fbab8b9431fd10862f6b0d6e6db58d93aa54a
wTristaan <t.joubert78980@gmail.com>
2025-06-28 16:34:05 +0200

    [ADD] about section

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { LandingComponent } from './landing/landing.component';
+import { AboutComponent } from './about/about.component';
 
 @Component({
   selector: 'app-body',
-  imports: [LandingComponent],
+  imports: [LandingComponent, AboutComponent],
   templateUrl: './body.component.html',
   styleUrl: './body.component.scss',
 })
