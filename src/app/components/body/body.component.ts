a74eb99955b1cdbb8eff868264ee66e708f82e90
wTristaan <t.joubert78980@gmail.com>
2025-06-28 16:58:29 +0200

    [ADD] services section

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { LandingComponent } from './landing/landing.component';
 import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
 
 @Component({
   selector: 'app-body',
-  imports: [LandingComponent, AboutComponent],
+  imports: [LandingComponent, AboutComponent, ServicesComponent],
   templateUrl: './body.component.html',
   styleUrl: './body.component.scss',
 })
